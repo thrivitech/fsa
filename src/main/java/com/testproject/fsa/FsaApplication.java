622b43a34f47b2bd9d2603a4adedeb3fa8a77b43
thrivitech <thrivitech@gmail.com>
2021-12-13 12:03:10 +0530

    first commit

---
+++ src/main/java/com/testproject/fsa/FsaApplication.java
@@ -0,0 +1,13 @@
+package com.testproject.fsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testproject/fsa/FsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testproject.fsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
